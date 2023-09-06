4b4e9acaee094f67f529c33940fd97502280f1de
umeh emmanuel <eumeh3882@gmail.com>
2023-09-06 06:44:51 -0700

    emmanuel

--- lawBE/index.ts
+++ lawBE/index.ts
@@ -1,1 +1,23 @@
-console.log("Start");
+import express, { Application } from "express"
+
+const app:Application =express()
+const port :number = 2854
+
+const server = app.listen(port,()=>{
+    console.log("")
+    console.log(`server listening on ${port}`)
+})
+
+
+process.on("uncaughtException",(reason:any)=>{
+    console.log("server is shutting down due to caught exception")
+    console.log(reason)
+})
+
+process.on("unhandledRejection",(error:any)=>{
+    console.log("server is shutting down due to unhandled rejection")
+    console.log(error)
+    server.close(()=>{
+        process.exit(1)
+    })
+})
