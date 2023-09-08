b508c89ce59a4fbad6376b4bafcdc6c5e3baae0e
Akinolajsh <akinolajsh@gmail.com>
2023-09-08 04:53:06 +0100

    start

---
+++ lawFE/src/Pages/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import GlobalButtonProps from '../global/GlobalButtonProps'
+
+const HomeScreen = () => {
+  return (
+    <div>
+        <GlobalButtonProps/>
+    </div>
+  )
+}
+
+export default HomeScreen
