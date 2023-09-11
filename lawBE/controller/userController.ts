f3b1ed8f32d505c6a528fb7cb67fd280f716ba23
UchennaAustine <uchennaaustine8@gmail.com>
2023-09-11 12:18:39 +0100

    lawDB

---
+++ lawBE/controller/userController.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+
+const prisma = new PrismaClient();
+
+export const viewInterpretation = async (req: Request, res: Response) => {
+  try {
+  } catch (error) {}
+};
