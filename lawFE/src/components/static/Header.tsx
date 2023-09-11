a767d47fd8d264f82c307164fd3f4be8d352f14b
unknown <emmanuelrich206@gmail.com>
2023-09-11 13:38:41 +0100

    richard

--- lawFE/src/components/static/Header.tsx
+++ lawFE/src/components/static/Header.tsx
@@ -21,30 +21,7 @@ window.addEventListener("scroll", onShow)
    <div>
     {
         show ? (
-            <div className="w-full h-[80px] flex justify-center items-center bg-white border-[black] border-b-[1px] z-[10] fixed duration-1000 transition-all">
-            {/* main */}
-            <div className="w-[90%] h-[100%] flex justify-between items-center">
-                {/* logo */}
-                <img src={logo} className="small:w-[50%]"/>
-                {/* Holder */}
-                <div className="flex items-center gap-[20px] text-[14px] ">
-                    <div className="flex items-center  gap-[20px]  small:hidden">
-                    <nav>Our story</nav>
-                    <nav>Membership</nav>
-                    <nav>Write</nav>
-                    <nav>Sign In</nav>
-                    </div>
-                    
-                    <Link to="/register">
-                 <GlobalButtonProps h="h-[40px]" w="w-[100px]" text="Get started" bg="bg-green-300" col="text-[white]" font="medium"/>
-                    </Link>
-                </div>
-            </div>
-            {/* main */}
-        </div>
-        ):
-        (
-            <div className="w-full h-[80px] flex justify-center items-center bg-[hsl(44,100%,55%)] border-[black] border-b-[1px] z-[10]">
+            <div className="w-full h-[80px] medium:h-[70px] small:h-[60px] flex justify-center items-center bg-white border-[black] border-b-[1px] z-[10] fixed duration-1000 transition-all">
             {/* main */}
             <div className="w-[90%] h-[100%] flex justify-between items-center">
                 {/* logo */}
@@ -60,6 +37,29 @@ window.addEventListener("scroll", onShow)
                     
                     <Link to="/register">
                  <GlobalButtonProps h="h-[40px]" w="w-[100px]" text="Get started" bg="bg-black" col="text-[white]" font="medium"/>
+                    </Link>
+                </div>
+            </div>
+            {/* main */}
+        </div>
+        ):
+        (
+            <div className="w-full h-[80px] medium:h-[70px] small:h-[60px] flex justify-center items-center bg-[hsl(44,100%,55%)] border-[black] border-b-[1px] z-[10]">
+            {/* main */}
+            <div className="w-[90%] h-[100%] flex justify-between items-center">
+                {/* logo */}
+                <img src={logo} className="small:w-[50%]"/>
+                {/* Holder */}
+                <div className="flex items-center gap-[20px] text-[14px] ">
+                    <div className="flex items-center  gap-[20px]  small:hidden">
+                    <nav>Our story</nav>
+                    <nav>Membership</nav>
+                    <nav>Write</nav>
+                    <nav>Sign In</nav>
+                    </div>
+                    
+                    <Link to="/register">
+                 <GlobalButtonProps h="h-[40px]" w="w-[100px]" text="Get started" bg="bg-black" col="text-[white]" font="medium" />
                     </Link>
                 </div>
             </div>
